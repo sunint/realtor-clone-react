90684faa5998cb29ee3fcf989bb8702738784dec
admin <admin@gmail.com>
2023-01-01 14:56:16 -0500

    created pages and routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,43 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import ForgotPassword from './pages/ForgotPassword'
+import Offer from './pages/Offer'
+import Profile from './pages/Profile'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
+
 function App() {
   return (
-    <h1 className='text-3xl font-bold underline text-red-600'>Hello world!</h1>
+    <>
+      <Router>
+        <Routes>
+          <Route
+            path='/'
+            element={<Home />}
+          />
+          <Route
+            path='/forgot-password'
+            element={<ForgotPassword />}
+          />
+          <Route
+            path='/offer'
+            element={<Offer />}
+          />
+          <Route
+            path='/profile'
+            element={<Profile />}
+          />
+          <Route
+            path='/sign-in'
+            element={<SignIn />}
+          />
+          <Route
+            path='/sign-up'
+            element={<SignUp />}
+          />
+        </Routes>
+      </Router>
+    </>
   )
 }
 
